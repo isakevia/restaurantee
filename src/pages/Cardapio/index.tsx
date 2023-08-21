57d24acc5c53fc55f01756362c17b58137c22c38
aryflavio <arianny.flavio@etec.sp.gov.br>
2023-08-21 12:20:23 -0300

    muitas coisas

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,5 @@
 import styles from './Cardapio.module.scss';
-import logo from '../../assets/logo.svg';
+import logo from 'assets/logo.svg';
 
 export default function Cardapio() {
     return (
@@ -7,6 +7,11 @@ export default function Cardapio() {
         <nav className={styles.menu}>
             <img src= {logo} alt="logo" />
         </nav>
+        <header className={styles.header}>
+            <div className={styles.header__text}>
+                A casa do código e da massa !!!
+            </div>
+        </header>
    </main>
     );
 }
